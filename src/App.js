af2f444228a4fda1d73a373347fcee9b7bb4972c
HarnoorBajwa <harnoor63@gmail.com>
2023-02-09 21:06:05 -0400

    added search component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BsFillCalendarDateFill } from "react-icons/bs";
+import {Container, Col, Row } from 'react-bootstrap';
+import Search from './components/Search';
+
 function App() {
   return (
     <div className="App">
-      <h1> Appointments </h1>
+      <Container>
+        <Row>
+          <Col >
+            <h1 className='text-center fw-light mt-3'><BsFillCalendarDateFill />Appointments </h1>
+          </Col>
+        </Row>
+        <Row className='justify-content-center'>
+          <Col md='4'> 
+            <Search />
+          </Col>
+        </Row>
+        <Row>    
+          <Col md={3}>
+            <h3>
+              Hello. bootstrap!
+            </h3>
+            </Col>
+            <Col md={3}>
+            <h3>
+              Hello. bootstrap!
+            </h3>
+            </Col>
+            <Col md={3}>
+            <h3>
+              Hello. bootstrap!
+            </h3>
+            </Col>
+            <Col md={3}>
+            <h3>
+              Hello. bootstrap!
+            </h3>
+            </Col>
+        </Row>
+      </Container>
+    
     </div>
   );
 }
